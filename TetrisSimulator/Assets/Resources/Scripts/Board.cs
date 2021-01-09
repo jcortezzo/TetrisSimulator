79c86af8b6e49ced22f0eadc521c195a10408eec
Nguyen Duong <ducnguyenduchao@gmail.com>
2021-01-09 02:29:51 -0800

    implemented basic board and tile

---
+++ TetrisSimulator/Assets/Resources/Scripts/Board.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public static Board Instance;
+
+    [SerializeField] private Tile[,] board;
+    [SerializeField] private Tile tilePrefab;
+
+    public int width;
+    public int height;
+
+    public float discoTime;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = new Tile[height, width];
+        GenerateBoard();
+    }
+
+    public void GenerateBoard()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            Vector2 tilePosition = this.transform.position - new Vector3(0, i, 0);
+            for (int j = 0; j < width; j++)
+            {
+                Tile tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity, transform);
+                tilePosition += Vector2.right;
+                board[i, j] = tile;
+            }
+        }
+    }
+
+    public void GenerateBoard(Texture2D levelTexture)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public Tile[,] GetBoard()
+    {
+        return board;
+    }
+}
---
+++ TetrisSimulator/Assets/Resources/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Color[] colors = { Color.red, Color.green, Color.blue, Color.yellow };
+    private static int counter = 0;
+    
+    
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    float timeElapsed;
+
+    void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+        Width = (int)sr.sprite.rect.width;
+        Height = (int)sr.sprite.rect.width;
+        sr.color = colors[(counter++) % colors.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeElapsed > Board.Instance.discoTime)
+        {
+            timeElapsed = 0;
+            sr.color = colors[Random.Range(0, colors.Length)];
+        }
+        timeElapsed += Time.deltaTime;
+    }
+
+
+}

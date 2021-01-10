474e5897a2f8519e32a5ef6d04dd9464e0e1a6aa
cortejon <cortejon@cs.washington.edu>
2021-01-09 21:08:08 -0800

    made 2 pieces

---
+++ TetrisSimulator/Assets/Resources/Scripts/JPiece.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JPiece : Piece
+{
+    protected override void Awake()
+    {
+        this.boundingBox = new bool[,] { { true, false, false },
+                                         { true, true,  true } };
+        ResetHeightAndWidth();
+    }
+}

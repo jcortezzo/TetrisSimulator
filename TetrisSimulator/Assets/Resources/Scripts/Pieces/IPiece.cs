59f1b3b2a13adff61f6a4b6aeda123c84ac834ec
cortejon <cortejon@cs.washington.edu>
2021-01-10 02:23:07 -0800

    can switch between pieces

---
+++ TetrisSimulator/Assets/Resources/Scripts/Pieces/IPiece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IPiece : Piece
+{
+    protected override void Awake()
+    {
+        this.boundingBox = new bool[,] { { true, true, true, true } };
+        ResetHeightAndWidth();
+        this.color = Color.cyan;
+    }
+
+}
---
+++ TetrisSimulator/Assets/Resources/Scripts/Pieces/ZPiece.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZPiece : Piece
+{
+    protected override void Awake()
+    {
+        this.boundingBox = new bool[,] { { true,  true, false },
+                                         { false, true, true } };
+        ResetHeightAndWidth();
+        this.color = Color.green;
+    }
+
+}
